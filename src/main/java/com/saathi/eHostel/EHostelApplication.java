5c72e9f0e15c5e219a7b218d3be8cbd0ea3d35d2
Saathi Paul <saathipaul@gmail.com>
2025-12-10 16:16:24 +0530

    Initial Commit

---
+++ src/main/java/com/saathi/eHostel/EHostelApplication.java
@@ -0,0 +1,13 @@
+package com.saathi.eHostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHostelApplication.class, args);
+	}
+
+}
